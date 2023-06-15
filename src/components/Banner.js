77dbfb7dff82eefb104eb6507f40d8d50a599c3d
Thanh Br <thanh.mathe@upos.vn>
2023-06-15 20:27:33 +0700

    update banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,16 +12,28 @@ import { TypeAnimation } from 'react-type-animation';
 
 const Banner = () => {
   return (
-    <section className='section' id='home'>
+    <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
       <div className='container mx-auto'>
-        <div className='flex flex-col gap-y-8 lg:flex-row'>
+        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
           {/* text */}
           <div className='flex-1 text-center font-secondary lg:text-left'>
-            <h1 className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>
+            <motion.h1 
+              variants={fadeIn('up', 0.3)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'
+            >
               Thanh <span>Ma</span>
-            </h1>
-            <div className='mb-6 text-[36px] lg:text-[60px] font-secondary 
-                            font-semibold uppercase leading-[1]'>
+            </motion.h1>
+            <motion.div 
+              variants={fadeIn('up', 0.4)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='mb-6 text-[36px] lg:text-[60px] font-secondary 
+                        font-semibold uppercase leading-[1]'
+            >
               <span className='text-white mr-4'>I am a</span>
               <TypeAnimation 
                 sequence={[
@@ -35,17 +47,35 @@ const Banner = () => {
                 wrapper='span'
                 repeat={Infinity}
               />
-            </div>
-            <p className='mb-8 max-w-lg mx-auto lg:mx-0'>
+            </motion.div>
+            <motion.p 
+              variants={fadeIn('up', 0.5)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='mb-8 max-w-lg mx-auto lg:mx-0'
+            >
               I have worked in this field of software for three years. 
               I specialize in building software that meets customer needs and I have a unique approach to doing this.
-            </p>
-            <div className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+            </motion.p>
+            <motion.div
+              variants={fadeIn('up', 0.6)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }} 
+              className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'
+            >
               <button className='btn btn-lg'>Contact me</button>
               <a href='#' className='text-gradient btn-link'>My Portfolio</a>
-            </div>
+            </motion.div>
             {/* socials */}
-            <div className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+            <motion.div
+              variants={fadeIn('up', 0.7)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'
+            >
               <a href='#'>
                 <FaGithub />
               </a>
@@ -55,12 +85,18 @@ const Banner = () => {
               <a href='#'>
                 <FaYoutube />
               </a>
-            </div>
+            </motion.div>
           </div>
           {/* images */}
-          <div>
+          <motion.div
+            variants={fadeIn('down', 0.5)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.7 }} 
+            className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]'
+          >
             <img src={Image} alt='image' />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
