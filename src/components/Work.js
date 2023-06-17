1a28ceb5c1613a349d0635f89883c67411fd27ef
Thanh Br <thanh.mathe@upos.vn>
2023-06-17 23:09:33 +0700

    update contact

--- src/components/Work.js
+++ src/components/Work.js
@@ -13,7 +13,13 @@ const Work = () => {
     <section className='section' id='work'>
       <div className='container mx-auto'>
         <div className='flex flex-col lg:flex-row gap-x-10'>
-          <div className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
+          <motion.div
+            variants={fadeIn('right', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{once: false, amount: 0.3}} 
+            className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'
+          >
             {/* text */}
             <div>
               <h2 className='h2 leading-tight text-accent'>
@@ -45,8 +51,14 @@ const Work = () => {
                 <span className='text-3xl text-white'>Project Title</span>
               </div>
             </div>
-          </div>
-          <div className='flex-1'>
+          </motion.div>
+          <motion.div 
+            variants={fadeIn('left', 0.2)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{once: false, amount: 0.3}} 
+            className='flex-1 flex flex-col gap-y-10'
+          >
             {/* image */}
             <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
               {/* overplay */}
@@ -65,7 +77,25 @@ const Work = () => {
                 <span className='text-3xl text-white'>Project Title</span>
               </div>
             </div>
-          </div>
+            {/* image */}
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              {/* overplay */}
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+              {/* img */}
+              <img src={Image3} 
+                    alt='image' 
+                  className='group-hover:scale-125 transition-all duration-500'
+              />
+              {/* pretitle */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-accent'>UI/UX Designer</span>
+              </div>
+              {/* title */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-500 z-50'>
+                <span className='text-3xl text-white'>Project Title</span>
+              </div>
+            </div>
+          </motion.div>
         </div>
       </div>
     </section>
